61642a88a2310ea1ad222e79bf63c07b1ff50293
Jason Qiu <jasonqiu93.com>
2026-07-11 00:07:50 +0000

    hoenstly dunno

--- dir/train_eval_loop.py
+++ dir/train_eval_loop.py
@@ -1,9 +1,3 @@
-"""
-Example program that uses the single-player MCTS algorithm to train an agent
-to master the HillClimbingEnvironment, in which the agent has to reach the
-highest point on a map.
-"""
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,33 +7,36 @@ from .mcts import execute_episode
 from .mcts import execute_episode_eval
 
 
-from .hill_climbing_example.policy import HillClimbingPolicy
-from .hill_climbing_example.hill_climbing_env import HillClimbingEnv
-
-def log(test_env, iteration, step_idx, total_rew):
-    """
-    Logs one step in a testing episode.
-    :param test_env: Test environment that should be rendered.
-    :param iteration: Number of training iterations so far.
-    :param step_idx: Index of the step in the episode.
-    :param total_rew: Total reward collected so far.
-    """
-    print()
-    print(f"Training Episodes: {iteration}")
-    test_env.render()
-    print(f"Step: {step_idx}")
-    print(f"Return: {total_rew}")
 
 
 
+
+# setup traininer ( which is just choose your policy and pass in paramaters to it
+# -------------
+#ig we'll just change this whenever we want to test different thing?
+from .hill_climbing_example.policy import HillClimbingPolicy as Policy
+from .hill_climbing_example.hill_climbing_env import HillClimbingEnv as Env
+#from .lattice_ffn_policy import Policy
+#from .2d_lattice_env_0.env import Env
+
+n_obs = 49 #... only needed for thier policy netowrk...
 
 n_actions = 4
-n_obs = 49
+trainer = Trainer(lambda: Policy(n_obs, 20, n_actions)) # how we initalize netowrk dependson our netwrok.....
 
-trainer = Trainer(lambda: HillClimbingPolicy(n_obs, 20, n_actions))
 network = trainer.step_model
 
-mem = ReplayMemory(200,
+#----------------
+
+# actual train_eval loop stuff is below here ig
+
+
+
+
+memory_size = 200
+
+
+mem = ReplayMemory(memory_size,
                    { "ob": np.long,
                      "pi": np.float32,
                      "return": np.float32},
@@ -47,14 +44,13 @@ mem = ReplayMemory(200,
                      "pi": [n_actions],
                      "return": []})
 
+
 def test_agent(iteration):
-    test_env = HillClimbingEnv()
     obs, pis, returns, total_reward, done_state, action_list= execute_episode_eval(network,
                                                                  32,
-                                                                 HillClimbingEnv)
-     
-
+                                                                Env)
     return total_reward
+
 
 def loop():
     value_losses = []
@@ -66,10 +62,8 @@ def loop():
             total_reward = 0
             for j in range(n):
                 total_reward+=test_agent(i)
-            print(f"avg reward: {total_reward/n}")
-            logits, p,v = network(np.array([i for i in range(49)]))
-            for i in range(7):
-                print(v[7*i:7*(i+1)])
+            
+            print(f"step{i}, avg reward: {total_reward/n}")
             
             plt.plot(value_losses, label="value loss")
             plt.plot(policy_losses, label="policy loss")
@@ -78,7 +72,7 @@ def loop():
 
         obs, pis, returns, total_reward, done_state = execute_episode(network,
                                                                  32,
-                                                                 HillClimbingEnv)
+                                                                 Env)
         mem.add_all({"ob": obs, "pi": pis, "return": returns})
 
         batch = mem.get_minibatch()
