d72f7ef0f558f58545b66c4ce1ce3ea159fc49a6
Jason Qiu <jasonqiu93.com>
2026-07-27 19:36:57 +0000

    okay so realized that default settings for pytorch is sequence first and not batch first (so in the positional encoding and transformer layer), we can change a paramter for the transformer, but not the positional encoding thing and we realized i don't feel confident we can mess with the positional encoding from hte tutorial so we are using some person's implementation instead as well now.

--- dir/encoder_policy.py
+++ dir/encoder_policy.py
@@ -5,42 +5,16 @@ from torch import nn, Tensor
 import numpy as n
 import torch.nn.functional as F
 
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using {device}")
-
-
-class PositionalEncoding(nn.Module):
-  def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
-      super().__init__()
-      self.dropout = nn.Dropout(p=dropout)
-
-      position = torch.arange(max_len).unsqueeze(1)
-      div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-      pe = torch.zeros(max_len, 1, d_model)
-      pe[:, 0, 0::2] = torch.sin(position * div_term)
-      pe[:, 0, 1::2] = torch.cos(position * div_term)
-      self.register_buffer('pe', pe)
-
-
-  def forward(self, x: Tensor) -> Tensor:
-      """
-      Arguments:
-          x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
-      """
-      x = x + self.pe[:x.size(0)]
-      return self.dropout(x)
-
+from positional_encodings.torch_encodings import PositionalEncoding1D, Summer
 
 
 class Policy(nn.Module):
   def __init__(self, num_encoder_layers, vector_dim, encoder_nhead, num_actions):
     super().__init__()
-    self.pos_emb= PositionalEncoding(vector_dim)
+    self.pos_enc= Summer(PositionalEncoding1D(vector_dim))
 
-    encoder_layer =nn.TransformerEncoderLayer(d_model=vector_dim, nhead=encoder_nhead)
+    encoder_layer =nn.TransformerEncoderLayer(d_model=vector_dim, nhead=encoder_nhead,batch_first=True)
     self.encoder = transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_encoder_layers, enable_nested_tensor=False)
-
 
     self.linear_p = nn.Linear(vector_dim, num_actions)
     self.linear_v = nn.Linear(vector_dim, 1)
@@ -49,14 +23,13 @@ class Policy(nn.Module):
     self.extra_colv =  nn.Parameter(torch.randn(vector_dim))
 
   def forward(self,x):
-    x= x.to(device)
     inp = torch.cat([x,
                     self.extra_colp.unsqueeze(0).expand(x.size(0), -1, -1),
                     self.extra_colv.unsqueeze(0).expand(x.size(0), -1, -1)
                     ],
                     dim=1
-                    ).float()
-    inp = self.pos_emb(inp)
+                    )
+    inp = self.pos_enc(inp)
     inp = self.encoder(inp)
 
     #compute logits and p
@@ -78,4 +51,4 @@ class Policy(nn.Module):
     obs = torch.from_numpy(obs)
     _, pi, v = self.forward(obs)
 
-    return pi.detach().cpu().numpy(), v.detach().cpu().numpy()
+    return pi.detach().numpy(), v.detach().numpy()
