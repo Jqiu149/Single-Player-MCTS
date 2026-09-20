afa1ebd8684faf8b2b4251861d6c6f95ffdd99a7
Jason Qiu <jasonqiu93.com>
2026-09-20 00:35:04 +0000

    added shear only actions env with gram matrix obs

---
+++ dir/envs/latticeEnv2D/gram_matrix_obs_shear_actions.py
@@ -0,0 +1,137 @@
+import numpy as np
+import random
+import	copy
+from functools import partial
+from scipy.stats import loguniform
+from ..static_env import StaticEnv
+from .. helper import *
+from .problem_specific_helpers import *
+
+
+start_obj_generator= None
+apply_step_penalty= None
+
+MAX_STEP = None
+
+
+#actions
+END = 0
+ADD_V0= 1
+SUB_V0= 2
+ADD_V1= 3
+SUB_V1= 4
+
+
+def get_obs_shape():
+	return [2, 2]
+
+
+
+#states will be list of...
+#2 np arrays of legnth 2: 2 linearly indep vectors of dim 2 defining the lattice
+#HIST_LEN many np arrays of length 2: the past HIST_LEN many vectors
+#	the magnitude of the smallest vector in the lattice
+#and a boolean 'done' that the agent can set to true to finish the episode
+
+
+class Env(StaticEnv):
+	n_actions= 5
+
+	@staticmethod
+	def next_state(state, action):
+		"""
+		Given the current state of the environment and the action that is
+		performed in that state, returns the resulting state.
+		:param state: Current state of the environment.
+		:param action: Action that is performed in that state.
+		:return: Resulting state.
+		"""
+
+		v0 = state[0]
+		v1 = state[1]
+
+		m = state[-2]
+		done = state[-1]
+
+		if(action == END):
+			new_v0 = v0
+			new_v1 = v1
+			done = True
+		elif(action== ADD_V0):
+			new_v0 = v0
+			new_v1 = v1 + v0
+		elif(action == SUB_V0):
+			new_v0 = v0
+			new_v1 = v1-v0
+		elif(action== ADD_V1):
+			new_v0 = v0+v1
+			new_v1 = v1
+		elif(action == SUB_V1):
+			new_v0 = v0 -v1
+			new_v1 = v1
+		else:
+			raise ValueError(f"given action, {action}, is unknown")
+
+		return [new_v0, new_v1, m, done]
+
+# i don't think we can do the step_idx thing for is done if we want to only give reward at the end after the stop button is used
+	@staticmethod
+	def is_done_state(state, step_idx):
+
+		"""
+		Given the state and the index of the current step, returns whether
+		that state is the end of an episode, i.e. a done state.
+		:param state: Current state.
+		:param step_idx: Index of the step at which the state occurred.
+		:return: True, if the step is a done state, False otherwise.
+		"""	
+		return state[-1] == True or step_idx >= MAX_STEP
+
+	@staticmethod
+	def initial_state():
+		"""
+		Returns the initial state of the environment.
+		"""		
+		start_basis= start_obj_generator()
+		smallest_vector = LagrangeReduce(start_basis[0], start_basis[1])[0]
+		smallest_m = np.linalg.norm(smallest_vector)
+	
+		return start_basis+[smallest_m, False]
+
+	@staticmethod
+	def get_obs_for_states(states):
+		"""
+		Some environments distinguish states and observations. An observation
+		can be a subset (e.g. in Poker, state is all cards in game, observation
+		is cards on player's hand) or superset of the state (i.e. observations
+		add additional information).
+		:param states: List of states.
+		:return: Numpy array of observations.
+		"""
+		x = np.array([ np.array(state[0:-2]) @ np.array(state[0:-2]).T for state in states],dtype=np.float32)
+
+		return x
+
+	@staticmethod
+	def get_return(state, step_idx):
+		"""
+		Returns the return that the agent has achieved so far when he is in
+		a given state after a given number of steps.
+		:param state: Current state that the agent is in.
+		:param step_idx: Index of the step at which the agent reached that
+		state.
+		:return: Return the agent has achieved so far.
+		"""
+
+		current_magnitude= min([np.linalg.norm(v) for v in state[0:2]])
+
+		score = apply_step_penalty( ( state[-2]/current_magnitude)**2, step_idx	)
+
+		return	score
+
+
+statistic_functions=  {
+		"lagrange_step_counts_updated": lagrange_step_counts,
+		"reached_min_magnitude": reached_min_mag
+		}
+
